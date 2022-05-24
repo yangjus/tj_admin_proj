02fdef15647a60069ebc6b822eb73de5c3742ac7
SerhiiMaltsev <sm5zj@virginia.edu>
2022-05-24 15:29:33 -0400

    login page front

--- src/login-page_components/Login.js
+++ src/login-page_components/Login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react"
-import { Checkbox, Grid, TextField, FormControlLabel, Paper, Button } from '@mui/material';
+import { Box, AppBar, Toolbar, Typography, Checkbox, Grid, TextField, FormControlLabel, Paper, Button } from '@mui/material';
+import { Link } from "react-router-dom";
+import './Login.css';
 
 const Login = (props) => {
     const [username, setUsername] = useState();
@@ -13,14 +15,29 @@ const Login = (props) => {
 
     return (
         <div className="Login">
-          <h1>Login Page</h1>
+          <Box sx={{ flexGrow: 1 }}>
+              <AppBar position="static">
+                  <Toolbar>
+                      <Typography variant="h6" component="div">
+                          TJ Admin Portal
+                      </Typography>
+                  </Toolbar>
+              </AppBar>
+          </Box>
+
+          <div className="LoginLabel">
+            <Typography variant="h4" component="div">
+                Login
+            </Typography>
+          </div>
+
           <div className="Username">
             <TextField label="Username" onChange={(e) => {setUsername(e.target.value)}}></TextField>
           </div>
           <div className="Password">
             <TextField label="Password" onChange={(e) => {setPassword(e.target.value)}} type={'password'}></TextField>
           </div>
-          <div className="Button">
+          <div className="LoginButton">
             <Button variant="contained" onClick={getCredentials}> Login </Button>
           </div>
         </div>
