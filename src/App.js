430dc5de6c26c167dd2c400011d820d392e758b1
yangjus <justiny_2013@yahoo.com>
2022-05-24 13:57:16 -0400

    templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './home-page_components/Home.js';
+import Login from './login-page_components/Login.js';
+import Error from './Error.js';
+import { Routes, Route } from 'react-router-dom';
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore";
+
+document.title = "TJ Elementary School Admin Portal";
 
 function App() {
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login db={db}/>}/>
+        <Route path="/home" element={<Home db={db}/>}/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/login-page_components/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Login = (props) => {
+
+    return (
+        <h1>Login Page</h1>
+    );
+};
+
+export default Login;
