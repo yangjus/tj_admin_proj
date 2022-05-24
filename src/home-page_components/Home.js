668ed1fa947c8ceaed52f992ee1006c001d40500
yangjus <justiny_2013@yahoo.com>
2022-05-24 15:58:33 -0400

    adjusted image and header

--- src/home-page_components/Home.js
+++ src/home-page_components/Home.js
@@ -7,15 +7,20 @@ const Home = (props) => {
 
     const btnSize = {
         maxWidth: "300px",
-        maxHeight: "300px",
+        maxHeight: "250px",
         minWidth: "300px",
-        minHeight: "300px",
+        minHeight: "250px",
     };
+
+    const photoSize = {
+        height: "200px",
+        width: "600px",
+    }
 
     return (
         <>
             <Navbar/>
-            <h1>Home Page</h1>
+            <h1>Home Portal</h1>
             <Grid container spacing={1}>
                 <Grid item xs={3}>
                     <Button variant='contained' fontSize="30" style={btnSize}>Student Directory</Button>
@@ -30,7 +35,10 @@ const Home = (props) => {
                     <Button variant='contained' fontSize="30" style={btnSize}>Calendar</Button>
                 </Grid>
             </Grid>
-            <img src={TJES} />
+            <br></br>
+            <Grid backgroundSize='contained'>
+                <img src={TJES} style={photoSize}/>
+            </Grid>
         </>
     );
 };
