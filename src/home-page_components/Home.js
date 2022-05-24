9521086e663c09a258e3d2136cc0e5ec495cf34b
yangjus <justiny_2013@yahoo.com>
2022-05-24 15:26:26 -0400

    border on buttons

--- src/home-page_components/Home.js
+++ src/home-page_components/Home.js
@@ -1,19 +1,35 @@
 import React from "react";
 import Navbar from "../Navbar.js";
-import {Grid, Button} from '@mui/material';
+import {Grid, Button, Card} from '@mui/material';
 
 const Home = (props) => {
+
+    const btn = {
+        border: "2px solid black",
+        backgroundColor: "white",
+        color: "dodgerblue",
+        padding: "14px 28px",
+        fontSize: "16px",
+        cursor: "pointer",
+        borderColor: "#2196F3",
+    };
 
     return (
         <>
             <Navbar/>
             <h1>Home Page</h1>
-            <Grid container spacing={5}>
-                <Grid item xs={6}>
-                    <Button>Testing</Button>
+            <Grid container spacing={1}>
+                <Grid item xs={3}>
+                    <Button style={btn}>Testing</Button>
                 </Grid>
-                <Grid item xs={6}>
-                    <Button>Testing</Button>
+                <Grid item xs={3}>
+                    <Button style={btn}>Testing</Button>
+                </Grid>
+                <Grid item xs={3}>
+                    <Button style={btn}>Testing</Button>
+                </Grid>
+                <Grid item xs={3}>
+                    <Button style={btn}>Testing</Button>
                 </Grid>
             </Grid>
         </>
