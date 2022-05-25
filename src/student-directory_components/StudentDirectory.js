ed5dbffd72e73a9d9915be4a8779be01c14046f0
Luke Xue <luqi9900@gmail.com>
2022-05-25 10:01:12 -0600

    added edit functions

--- src/student-directory_components/StudentDirectory.js
+++ src/student-directory_components/StudentDirectory.js
@@ -3,25 +3,42 @@ import Navbar from '../Navbar.js';
 import db from '../firebase.js'
 import { getFirestore, collection, addDoc, doc, getDocs, updateDoc, increment } from "firebase/firestore";
 
-const [students, setStudents] = useState([])
-
-useEffect(() => {
-  const students = []
-  getDocs(collection(db, "students"))
-  .then((allStudents) => allStudents.forEach((student) => students.push({id: student.id, ...student.data()}))
-  )
-  setStudents(students)
-}, [db])
-
-const editName = () => {
-
-}
-
-const editBirth = () => {
-  
-}
 
 const StudentDirectory = () => {
+
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    const students = []
+    getDocs(collection(db, "students"))
+    .then((allStudents) => allStudents.forEach((student) => students.push({id: student.id, ...student.data()}))
+    )
+    setStudents(students)
+  }, [db])
+  
+  const editFirstName = (studentID, newFirstName) => {
+    updateDoc(doc(db, "students", studentID)), {
+      firstname: newFirstName
+    }
+  }
+
+  const editLastName = (studentID, newLastName) => {
+    updateDoc(doc(db, "students", studentID)), {
+      lastname: newLastName
+    }
+  }
+  
+  const editBirth = (studentID, newBirthday) => {
+    updateDoc(doc(db, "students", studentID)), {
+      birthday: newBirthday
+    }
+  }
+
+  const editGrade = (studentID, newGrade) => {
+    updateDoc(doc(db, "students", studentID)), {
+      grade: newGrade
+    }
+  }
 
     return (
         <>
