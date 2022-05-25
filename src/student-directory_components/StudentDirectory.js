461ff87d696467d4b9e9741903fb166852535eff
yangjus <justiny_2013@yahoo.com>
2022-05-25 11:37:36 -0400

    icon package

--- src/student-directory_components/StudentDirectory.js
+++ src/student-directory_components/StudentDirectory.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import Navbar from '../Navbar.js';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
 
 const StudentDirectory = () => {
 
     return (
         <>
             <Navbar />
-            <h1>This is the Student Directory page</h1>
+            <h1>Student Directory</h1>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {[0, 1, 2, 3].map((value) => {
+
+                return (
+                <ListItem
+                    key={value}
+                    secondaryAction={
+                    <IconButton edge="end" aria-label="comments">
+                        <EditIcon />
+                    </IconButton>
+                    }
+                    disablePadding
+                >
+                </ListItem>
+                );
+            })}
+            </List>
         </>
     );
 }
