7c7be6faa3caeea1bac0794dd1b0da0396856bd9
yangjus <justiny_2013@yahoo.com>
2022-05-25 11:58:02 -0400

    merges

--- src/student-directory_components/StudentDirectory.js
+++ src/student-directory_components/StudentDirectory.js
@@ -1,60 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../Navbar.js';
-<<<<<<< HEAD
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
-=======
+import Grid from '@mui/material/Grid';
 import db from '../firebase.js'
 import { getFirestore, collection, addDoc, doc, getDocs, updateDoc, increment } from "firebase/firestore";
 
-const [students, setStudents] = useState([])
-
-useEffect(() => {
-  const students = []
-  getDocs(collection(db, "students"))
-  .then((allStudents) => allStudents.forEach((student) => students.push({id: student.id, ...student.data()}))
-  )
-  setStudents(students)
-}, [db])
-
-const editName = () => {
-
-}
-
-const editBirth = () => {
-  
-}
->>>>>>> origin/lukebranch
-
 const StudentDirectory = () => {
+
+    const [students, setStudents] = useState([])
+
+    useEffect(() => {
+    const students = []
+    getDocs(collection(db, "students"))
+    .then((allStudents) => allStudents.forEach((student) => students.push({id: student.id, ...student.data()}))
+    )
+    setStudents(students)
+    }, [db])
+
+    const editFirstName = (studentID, newFirstName) => {
+        updateDoc(doc(db, "students", studentID)), {
+            firstname: newFirstName
+        }
+    }
+    
+    const editLastName = (studentID, newLastName) => {
+        updateDoc(doc(db, "students", studentID)), {
+            lastname: newLastName
+        }
+    }
+    
+    const editBirth = (studentID, newBirthday) => {
+        updateDoc(doc(db, "students", studentID)), {
+            birthday: newBirthday
+        }
+    }
+    
+    const editGrade = (studentID, newGrade) => {
+        updateDoc(doc(db, "students", studentID)), {
+            grade: newGrade
+        }
+    }
 
     return (
         <>
             <Navbar />
             <h1>Student Directory</h1>
-            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            {[0, 1, 2, 3].map((value) => {
-
-                return (
-                <ListItem
-                    key={value}
-                    secondaryAction={
+            <Grid justify="center">
+                <List>
+                    <ListItem secondaryAction={
                     <IconButton edge="end" aria-label="comments">
                         <EditIcon />
-                    </IconButton>
-                    }
-                    disablePadding
-                >
-                </ListItem>
-                );
-            })}
-            </List>
+                    </IconButton> } disablePadding>
+                        Hello
+                    </ListItem>
+                    <ListItem>
+                        Hello2
+                        <EditIcon edge="end"/>
+                    </ListItem>
+                </List>
+            </Grid>
         </>
     );
 }
