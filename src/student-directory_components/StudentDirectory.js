421f360f1090301944144f553f5adacd1f2e92b6
Luke Xue <luqi9900@gmail.com>
2022-05-25 09:36:12 -0600

    added database call

--- src/student-directory_components/StudentDirectory.js
+++ src/student-directory_components/StudentDirectory.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Navbar.js';
+import db from '../firebase.js'
+import { getFirestore, collection, addDoc, doc, getDocs, updateDoc, increment } from "firebase/firestore";
+
+const [students, setStudents] = useState([])
+
+useEffect(() => {
+  const students = []
+  getDocs(collection(db, "students"))
+  .then((allStudents) => allStudents.forEach((student) => students.push({id: student.id, ...student.data()}))
+  )
+  setStudents(students)
+}, [db])
+
+const editName = () => {
+
+}
+
+const editBirth = () => {
+  
+}
 
 const StudentDirectory = () => {
 
